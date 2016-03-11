bafff0c68406a7fe26b3959bf56e67db725561b3
xiaoluoji <zhonghaicheng@gmail.com>
2016-03-12 04:10:38 +0800

    添加资源

--- ArcDB/Properties/Resources.Designer.cs
+++ ArcDB/Properties/Resources.Designer.cs
@@ -63,6 +63,36 @@ namespace ArcDB.Properties {
         /// <summary>
         ///   查找 System.Byte[] 类型的本地化资源。
         /// </summary>
+        internal static byte[] HtmlAgilityPack {
+            get {
+                object obj = ResourceManager.GetObject("HtmlAgilityPack", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Byte[] 类型的本地化资源。
+        /// </summary>
+        internal static byte[] ImageListView {
+            get {
+                object obj = ResourceManager.GetObject("ImageListView", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Byte[] 类型的本地化资源。
+        /// </summary>
+        internal static byte[] Magick_NET_Q16_AnyCPU {
+            get {
+                object obj = ResourceManager.GetObject("Magick_NET_Q16_AnyCPU", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Byte[] 类型的本地化资源。
+        /// </summary>
         internal static byte[] MurmurHash {
             get {
                 object obj = ResourceManager.GetObject("MurmurHash", resourceCulture);
